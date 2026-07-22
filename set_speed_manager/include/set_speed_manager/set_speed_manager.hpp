d4ea53d20074c028933e084810e2c52cef9a60a6
willd182 <willd182@my.erau.edu>
2026-07-22 18:40:41 +0000

    Initial Software

---
+++ set_speed_manager/include/set_speed_manager/set_speed_manager.hpp
@@ -0,0 +1,20 @@
+#ifndef SET_SPEED_MANAGER_HPP
+#define SET_SPEED_MANAGER_HPP
+
+class SetSpeedManager
+{
+    public:
+        SetSpeedManager();
+
+        int getSpeed() const;
+        void setSpeed(int speed);
+        void increment();
+        void decrement();
+    
+    private:
+    int set_speed_;
+    int min_speed_;
+    int max_speed_;
+};
+
+#endif
---
+++ set_speed_manager/src/set_speed_manager.cpp
@@ -0,0 +1,27 @@
+#include "set_speed_manager/set_speed_manager.hpp"
+#include <algorithm>
+
+SetSpeedManager::SetSpeedManager()
+: set_speed_(0), min_speed_(0), max_speed_(0)
+{
+}
+
+int SetSpeedManager::getSpeed() const
+{
+    return set_speed_;
+}
+
+void SetSpeedManager::setSpeed(int speed)
+{
+    set_speed_ = std::clamp(speed, min_speed_, max_speed_);
+}
+
+void SetSpeedManager::increment()
+{
+    setSpeed(set_speed_ + 5);
+}
+
+void SetSpeedManager::decrement()
+{
+    setSpeed(set_speed_ - 5);
+}
